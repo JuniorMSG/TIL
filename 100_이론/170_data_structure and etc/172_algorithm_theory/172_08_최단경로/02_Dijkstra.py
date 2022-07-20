39ee807e14a3d5cb1486c28661eb957293c216a6
MSGDev <milk6645@naver.com>
2022-07-20 23:59:10 +0900

    dijkstra algorithm

---
+++ 100_이론/170_data_structure and etc/172_algorithm_theory/172_08_최단경로/02_Dijkstra.py
@@ -0,0 +1,39 @@
+# 172_algorithm_theory
+## 02_Dijkstra.py
+"""
+    참고: heapq 라이브러리 활용을 통해 우선순위 큐 사용하기
+    - 데이터가 리스트 형태일 경우, 0번 인덱스를 우선순위로 인지, 우선순위가 낮은 순서대로 pop 할 수 있음
+    최소 heap의 구조를 가진 코드를 쓸 수 있다.
+"""
+import heapq
+
+queue = []
+
+heapq.heappush(queue, [2, 'A'])
+heapq.heappush(queue, [5, 'B'])
+heapq.heappush(queue, [1, 'C'])
+heapq.heappush(queue, [7, 'D'])
+print(queue)
+for index in range(len(queue)):
+    print (heapq.heappop(queue))
+
+
+# 1. 그래프를 자료구조화 시킨다.
+mygraph = {
+    'A': {'B': 8, 'C': 1, 'D': 2},
+    'B': {},
+    'C': {'B': 5, 'D': 2},
+    'D': {'E': 3, 'F': 5},
+    'E': {'F': 1},
+    'F': {'A': 5}
+}
+
+def dijkstra(graph, start):
+    # 출발점과 최단거리를 보유하는 배열 생성
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    print(distances)
+
+    print(1234)
+
+dijkstra(mygraph, 'A')
