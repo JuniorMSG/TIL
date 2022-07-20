21301cbcca6979f72615006a0f9ed62b6fccf3d5
MSGDev <milk6645@naver.com>
2022-07-21 02:11:47 +0900

    최소신장트리

--- 100_이론/170_data_structure and etc/172_algorithm_theory/172_08_최단경로/02_Dijkstra.py
+++ 100_이론/170_data_structure and etc/172_algorithm_theory/172_08_최단경로/02_Dijkstra.py
@@ -32,8 +32,30 @@ def dijkstra(graph, start):
     # 출발점과 최단거리를 보유하는 배열 생성
     distances = {node: float('inf') for node in graph}
     distances[start] = 0
-    print(distances)
+    # 우선순위 큐
+    queue = []
+    heapq.heappush(queue, [distances[start], start])
+    ## 초기화 끝
 
-    print(1234)
 
-dijkstra(mygraph, 'A')
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+
+        if distances[current_node] < current_distance:
+            continue
+
+        for adjacent, weight in graph[current_node].items():
+            distance = current_distance + weight
+
+            if distance < distances[adjacent]:
+                distances[adjacent] = distance
+                heapq.heappush(queue, [distance, adjacent])
+
+    return distances
+
+
+
+
+print(dijkstra(mygraph, 'A'))
+print(dijkstra(mygraph, 'B'))
+print(dijkstra(mygraph, 'C'))
